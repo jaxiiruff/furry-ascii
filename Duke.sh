6261b0cfa3978b229d89b46e473a95e57c262b88
Jack <37735330+JaxiiRuff@users.noreply.github.com>
2022-05-21 03:15:10 +0000

    Added Bad Dragon Ascii Art

---
+++ Duke.sh
@@ -0,0 +1,20 @@
+                                 +7
+                              z*${c3}DD${c1}M
+                           ,/'${c3}DDD${c1}F
+                         +"${c3}DDDDD${c1}A
+                      ;*,${c3}DDDDDD${c1}/
+                    <L${c3}DDDDDDDDD${c1}M
+                 x*` "*"""""""****"1
+              ,A",               w<"
+            +"                ~Fn~~~~~~y
+          ,*      x                   7
+         /     .#F                 +*"
+      xC`            ${c2},e@@@eeew, ${c1}'?""""A 
+   ,+^             -=${c2}dDDDMDDDDDDD@e  ${c1},f
+ y^               y=${c2}NDMDDDDDDDDDDDD]${c1}K*
+ F           ,+#N${c2}]]DDDDDDDDDDDDDD${c1}H^
+f        w#N]${c2}]DDDDDDDDDDDDDDD${c1}/DM
+\\    ,w=M${c2}DDDDDDDDDDDDDDD${c1}DDDH*
+ V+N]${c2}]DDDDD${c1}j]1H*5??"""``
+   ""**H*"* 
+
