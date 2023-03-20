903fbb72067e75d97c7a50eb280d79272115572f
JaxiiRuff <37735330+JaxiiRuff@users.noreply.github.com>
2023-03-20 04:23:08 +0000

    added macchina version of ascii

---
+++ duke-macchina.sh
@@ -0,0 +1,20 @@
+                                 +7
+                              z*DD$
+                           ,/'$DDD$
+                         +"$DDDDD$
+                      ;*,$DDDDDD$
+                    <L$DDDDDDDDD$
+                 x*` "*"""""""****"1
+              ,A",               w<"
+            +"                ~Fn~~~~~~y
+          ,*      x                   7
+         /     .#F                 +*"
+      xC`             ,e@@@eeew,'?""""A
+   ,+^             -=dDDDMDDDDDDD@e  $,f
+ y^               y=NDMDDDDDDDDDDDD]K*
+ F           ,+#N]]DDDDDDDDDDDDDDH^
+f        w#N]]DDDDDDDDDDDDDDD/DM
+\\    ,w=MDDDDDDDDDDDDDDDDDDH*
+ V+N]]DDDDDj]1H*5??"""``
+   ""**H*"* 
+
